32ca95635022ff9522ec4995aca16fe82a86d2b9
sungdongyoon <ehdtjd0906@naver.com>
2023-11-26 15:35:55 +0900

    react native setting

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HI! My name is Yoon Dong Sung</Text>
       <StatusBar style="auto" />
     </View>
   );
