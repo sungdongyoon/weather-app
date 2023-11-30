399d9de50ce722ef91eae8314f9dce2d0a5b5fa6
sungdongyoon <ehdtjd0906@naver.com>
2023-12-01 00:24:02 +0900

    linear-gradient 설치 및 적용

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const {width: SCREEN_WIDTH} = Dimensions.get("window");
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <LinearGradient colors={["#f6d365", "#fda085"]} style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
@@ -30,13 +31,13 @@ export default function App() {
           <Text style={styles.description}>Sunny</Text>
         </View>
       </ScrollView>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, backgroundColor: "orange"
+    flex: 1,
   },
   city: {
     flex: 1,
