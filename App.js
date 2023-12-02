aa35a9562f57fe103b35d00f30e85c400cd032a4
sungdongyoon <ehdtjd0906@naver.com>
2023-12-03 01:16:07 +0900

    위치정보 불러오기

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 const {width: SCREEN_WIDTH} = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async() => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted) {
+      setOk(false);
+    }
+    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5});
+    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+    setCity(`${location[0].city} ${location[0].district}`);
+  }
+  useEffect(() => {
+    ask();
+  }, [])
   return (
     <LinearGradient colors={["#f6d365", "#fda085"]} style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView horizontal pagingEnabled indicatorStyle='white' contentContainerStyle={styles.weather}>
         <View style={styles.day}>
