cd8d8cc4603bb74ef1c5ff23c6e8811d6e208213
sungdongyoon <ehdtjd0906@naver.com>
2023-12-06 16:02:40 +0900

    퍼블리싱 및 현재 날씨 작업

--- App.js
+++ App.js
@@ -22,6 +22,9 @@ export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
+
+  const [currentWeather, setCurrentWeather] = useState([]);
+
   const getWeather = async() => {
     const {granted} = await Location.requestForegroundPermissionsAsync();
     if(!granted) {
@@ -30,20 +33,34 @@ export default function App() {
     const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5});
     const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
     setCity(`${location[0].city} ${location[0].district}`);
-    const resopnse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    const json = await resopnse.json();
-    const data = await json.list.filter((el) => el.dt_txt.includes("00:00:00"));
-    setDays(data);
-    console.log(days);
+
+    const weeklyWeatherAPI = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    const weeklyWeatherJson = await weeklyWeatherAPI.json();
+    const weeklyWeatherData = await weeklyWeatherJson.list.filter((el) => el.dt_txt.includes("00:00:00"));
+    setDays(weeklyWeatherData);
+
+    const currentWeatherAPI = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    const currentWeatherJson = await currentWeatherAPI.json();
+    const currentWeatherData = await currentWeatherJson;
+    setCurrentWeather(currentWeatherData);
   }
 
   useEffect(() => {
     getWeather();
+    console.log(currentWeather);
   }, [])
+
   return (
     <LinearGradient colors={["#f6d365", "#fda085"]} style={styles.container}>
       <View style={styles.city}>
+        <Text style={styles.title}>주간 날씨</Text>
         <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <View style={styles.current}>
+        <Text style={styles.currentTitle}>현재 날씨</Text>
+        <Text>아이콘</Text>
+        <Text style={styles.currentTemp}>{currentWeather.main.temp}°</Text>
+        <Text style={styles.currentDesc}>Rainy</Text>
       </View>
       <ScrollView horizontal pagingEnabled indicatorStyle='white' contentContainerStyle={styles.weather}>
         {days.length === 0 ?
@@ -52,10 +69,10 @@ export default function App() {
           </View> :
           days.map((weather, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.date}>{weather.dt_txt}</Text>
+              <Fontisto name={icons[weather.weather[0].main]} size={60} color="black"/>
+              <Text style={styles.date}>{weather.dt_txt.substring(0, weather.dt_txt.indexOf("00"))}</Text>
               <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
-                <Text style={styles.temp}>{Number(weather.main.temp).toFixed(1)}℃</Text>
-                <Fontisto name={icons[weather.weather[0].main]} size={60} color="black"/>
+                <Text style={styles.temp}>최저:{Number(weather.main.temp_min).toFixed(1)}°  최고:{Number(weather.main.temp_max).toFixed(1)}°</Text>
               </View>
               <Text style={styles.description}>{weather.weather[0].main}</Text>
             </View>
@@ -71,29 +88,50 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   city: {
-    flex: 1,
+    flex: 1.5,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingLeft: 20,
   },
   cityName: {
-    fontSize: 40,
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 50,
+  },
+  current: {
+    flex: 1,
+    justifyContent: "center",
+    paddingLeft: 20,
+  },
+  currentTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  currentTemp: {
+    fontSize: 20,
+  },
+  currentDesc: {
+    fontSize: 25,
   },
   weather: {
-
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingLeft: 20,
   },
   temp: {
-    fontSize: 100,
-    marginTop: 50,
+    fontSize: 20,
+    marginTop: 10,
   },
   description: {
-    fontSize: 30,
-    marginTop: -10,
+    fontSize: 25,
+    marginTop: 10,
   },
   date: {
-    fontSize: 20,
+    fontSize: 15,
   }
 })
